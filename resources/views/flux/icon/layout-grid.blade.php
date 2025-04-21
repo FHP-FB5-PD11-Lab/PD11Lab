18908b885ff051372f84f052f84e5f2f9887829a
McNamara <holgerehrmann@gmail.com>
2025-04-21 15:58:46 +0200

    Fixed unknown class Flux

--- resources/views/flux/icon/layout-grid.blade.php
+++ resources/views/flux/icon/layout-grid.blade.php
@@ -1,5 +1,4 @@
 {{-- Credit: Lucide (https://lucide.dev) --}}
-
 @props([
     'variant' => 'outline',
 ])
@@ -9,37 +8,29 @@
         throw new \Exception('The "solid" variant is not supported in Lucide.');
     }
 
-    $classes = Flux::classes('shrink-0')->add(
-        match ($variant) {
-            'outline' => '[:where(&)]:size-6',
-            'solid' => '[:where(&)]:size-6',
-            'mini' => '[:where(&)]:size-5',
-            'micro' => '[:where(&)]:size-4',
-        },
-    );
-
     $strokeWidth = match ($variant) {
         'outline' => 2,
-        'mini' => 2.25,
-        'micro' => 2.5,
+        'mini'    => 2.25,
+        'micro'   => 2.5,
+        default   => 2,
     };
 @endphp
 
 <svg
-    {{ $attributes->class($classes) }}
-    data-flux-icon
-    xmlns="http://www.w3.org/2000/svg"
-    viewBox="0 0 24 24"
-    fill="none"
-    stroke="currentColor"
-    stroke-width="{{ $strokeWidth }}"
-    stroke-linecap="round"
-    stroke-linejoin="round"
-    aria-hidden="true"
-    data-slot="icon"
+    {{ $attributes->merge([
+        'xmlns'            => 'http://www.w3.org/2000/svg',
+        'viewBox'          => '0 0 24 24',
+        'fill'             => 'none',
+        'stroke'           => 'currentColor',
+        'stroke-width'     => $strokeWidth,
+        'stroke-linecap'   => 'round',
+        'stroke-linejoin'  => 'round',
+        'aria-hidden'      => 'true',
+        'data-slot'        => 'icon',
+    ]) }}
 >
-    <rect width="7" height="7" x="3" y="3" rx="1" />
-    <rect width="7" height="7" x="14" y="3" rx="1" />
-    <rect width="7" height="7" x="14" y="14" rx="1" />
-    <rect width="7" height="7" x="3" y="14" rx="1" />
+    <rect x="3"  y="3"  width="7" height="7" rx="1" />
+    <rect x="14" y="3"  width="7" height="7" rx="1" />
+    <rect x="14" y="14" width="7" height="7" rx="1" />
+    <rect x="3"  y="14" width="7" height="7" rx="1" />
 </svg>
